e2af0824b98e4b15ad8d67173bc4246d10df3daa
Hershi Shor <e0548597918@gmail.com>
2025-05-21 15:21:45 +0300

    new weapon interface

---
+++ Iweapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_opertions
+{
+    internal interface Iweapon
+    {
+        string Name { get; set; }
+        int PowerLevel { get; set; }
+        string EffectiveFor { get; set; }
+
+    }
+}
