aa9d8feb06ade674e5d51063c4d891347afb7e31
Hershi Shor <e0548597918@gmail.com>
2025-05-21 15:31:41 +0300

    new IBomberWeapon interface

---
+++ IBomberWeapon.cs
@@ -0,0 +1,9 @@
+﻿public interface IBomberWeapon
+{
+    int Bombweight { get; set; }
+    int ThrowsInABlow { get; set; }
+    int FuelSupply { get; set; }
+    string BombBalance { get; set; }
+
+}
+
--- Iweapon.cs
+++ Iweapon.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IDF_opertions
+﻿public interface Iweapon
 {
-    internal interface Iweapon
-    {
-        string Name { get; set; }
-        int PowerLevel { get; set; }
-        string EffectiveFor { get; set; }
+    string Name { get; set; }
+    int PowerLevel { get; set; }
+    string EffectiveFor { get; set; }
 
-    }
 }
+
